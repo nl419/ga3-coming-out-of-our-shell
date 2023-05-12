61a9059b9ad9221ebb4c0193c611fcf58015e68a
Vidhi Sharma <vs503@cam.ac.uk>
2023-05-12 10:59:29 +0100

    commit 1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 # Config
 L_tube = 0.35
 d_i = 0.006
+d_o = 0.008
 
 # Properties
 rho = 990.1
@@ -24,8 +25,16 @@ del_p_tube = 0.5 * rho * V_tube ** 2 * (f * L_tube / d_i + K_e + K_c) \
     + 0.5 * rho * V_noz ** 2
 
 ## Pressure drop for shell side
+D_sh = 0.2 #for now
+Y = 0.014 #for now
+N = 13 #for now
+N_b = 9 #for now
+d_o = 0.008
+B = L_tube/(N_b + 1)
+A_sh = (D_sh/Y)*(Y - d_o)*B
 
-
+V_sh = 2 #for now
+Re_sh = 
 # Heat transfer coeff
 
 # Q
