c086ff8a46f76b0efa8bdeb5b1be737508f665e8
niklebedenko <98320255+niklebedenko@users.noreply.github.com>
2023-05-30 20:58:46 +0100

    overhaul mass estimate

--- mass_estimate.py
+++ mass_estimate.py
@@ -6,55 +6,68 @@ rho_acrylic_pipe = 0.65 # kg / m
 m_nozzle = 0.025        # kg
 rho_abs_sheet = 2.39    # kg / m2
 rho_3D_printed = 1150   # kg / m3
-m_o_ring = 5.3e-3       # kg
+m_big_o_ring = 5.3e-3   # kg
+m_small_o_ring = 0.8e-3 # kg   
 
-mass_limit = 1
-tube_length_limit_one_tube_pass = 0.35 - 40e-3 * 2 - 1.5e-3 * 4
-tube_length_limit_even_tube_pass = 0.35 - 40e-3 - 20e-3 - 1.5e-3 * 4
+mass_limit = 1.00
+tube_length_limit_one_tube_pass = 0.35 - 40e-3 * 2 - 8e-3 * 2 + 5.5e-3 * 2
+tube_length_limit_even_tube_pass = 0.35 - 40e-3 - 20e-3 - 8e-3 * 2 + 5.5e-3 * 2
 
 # Calculate mass of unchanging parts
 def _constant_mass(num_shell_passes, num_tube_passes, num_tubes, num_baffles):
-    # assert(num_shell_passes == 1 or num_shell_passes == 2)
-    # assert(num_tube_passes == 1 or num_tube_passes == 2 or num_tube_passes == 4)
-    #assert(num_tubes % num_tube_passes == 0)
-
     result = 0
 
     # End plates
-    d_endplate = 69e-3  # m
-    h_endplate = 4.5e-3 # m
-    A_endplate = np.pi * d_endplate**2 / 4 
-    result += 2 * rho_3D_printed * h_endplate * A_endplate
+    V_endplate = 18592.862e-9 # m^3
+    result += 2 * rho_3D_printed * V_endplate
 
-    # Header-shell separators
-    h_separator = h_endplate
-    od_tube = 8e-3
-    A_tubes = num_tubes * np.pi * od_tube**2 / 4
-    A_separator = A_endplate - A_tubes
-    result += 2 * rho_3D_printed * h_separator * A_separator
+    # Tubesheet
+    V_tubesheet = 26080.357e-9 # m^3
+    V_hole = 565.047e-9 # m^3
+    
+    result += 2 * rho_3D_printed * (V_tubesheet - num_tubes * V_hole)
+
+    # Length of copper stuck in the tubesheet
+    result += 2 * 8e-3 * num_tubes * rho_copper_tube
 
     # O-rings
-    result += 6 * m_o_ring
+    result += 6 * m_big_o_ring
+    result += 2 * num_tubes * m_small_o_ring
 
     # Nozzles
     result += 4 * m_nozzle
 
     # Baffles
-    h_baffle = 1.5e-3
-    # Conservative estimate, neglect the missing mass of the baffle cut
-    A_baffle = A_separator
-    result += num_baffles * rho_3D_printed * h_baffle * A_baffle
+    baffle_area_fraction = 2382.797 / 3186.902
+    A_baffle = 3186.902e-6 # m^2
+    A_hole = np.pi * 4e-3 ** 2
+    result += num_baffles * rho_abs_sheet * (A_baffle - num_tubes * A_hole) * baffle_area_fraction
+
+    # Header separator
+    half_width_separator = 63.7e-3 / 2
+    if num_tube_passes == 1:
+        num_separator_halves_front = 0
+        num_separator_halves_back = 0
+    elif num_tube_passes == 2:
+        num_separator_halves_front = 2
+        num_separator_halves_back = 0
+    elif num_tube_passes == 3:
+        num_separator_halves_front = 2
+        num_separator_halves_back = 2
+    else:
+        # Assume num_tube_passes == 4. Can't be bothered to implement more.
+        num_separator_halves_front = 3
+        num_separator_halves_back = 2
 
     # Front header
     l_small_header = 20e-3
     l_large_header = 40e-3
-    result += l_large_header * rho_acrylic_pipe
+    result += l_large_header * (rho_acrylic_pipe + num_separator_halves_front * half_width_separator * rho_abs_sheet)
     # Rear header
-    if num_tube_passes == 1:
-        result += l_large_header * rho_acrylic_pipe
+    if num_tube_passes % 2 == 1:
+        result += l_large_header * (rho_acrylic_pipe + num_separator_halves_back * half_width_separator * rho_abs_sheet)
     else:
-        result += l_small_header * rho_acrylic_pipe
-    # TODO account for header separators in case of multiple tube passes
+        result += l_small_header * (rho_acrylic_pipe + num_separator_halves_back * half_width_separator * rho_abs_sheet)
 
     # Note: the fixed mass is the same, regardless of num_shell_passes
     return result
@@ -65,7 +78,7 @@ def calculate_tube_length_baffle_spacing(num_shell_passes, num_tube_passes, num_
     rho_shell_tubes = rho_copper_tube * num_tubes + rho_acrylic_pipe
     L_shell_header = 40e-3
 
-    if num_tube_passes == 1:
+    if num_tube_passes % 2 == 1:
         tube_length_limit = tube_length_limit_one_tube_pass
     else:
         tube_length_limit = tube_length_limit_even_tube_pass
@@ -75,10 +88,9 @@ def calculate_tube_length_baffle_spacing(num_shell_passes, num_tube_passes, num_
         tube_length = min(tube_length, tube_length_limit)
         baffle_length = tube_length - L_shell_header * 2
     else:
-        h_shell_separator = 1.5e-3
         width_shell_separator = 63.7e-3
         # Mass of shell separator per unit length
-        rho_shell_separator = rho_3D_printed * h_shell_separator * width_shell_separator
+        rho_shell_separator = rho_abs_sheet * width_shell_separator
         # Note: last 20mm of shell are unseparated
         L_unseparated = 20e-3
         mass_unseparated = L_unseparated * (rho_copper_tube * num_tubes + rho_acrylic_pipe)
@@ -87,8 +99,6 @@ def calculate_tube_length_baffle_spacing(num_shell_passes, num_tube_passes, num_
         baffle_length = tube_length - L_unseparated - L_shell_header
     baffle_spacing = baffle_length / (num_baffles - 1) # Note: breaks if num_baffles = 1. So don't do that.
     return tube_length, baffle_spacing
-
-# TODO test that this gives reasonable results
 
 if __name__ == "__main__":
     num_tube_passes = 1
